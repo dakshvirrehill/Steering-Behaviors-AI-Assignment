f9b2bcc63618b69a50f9876aac124958895e9ee0
Dakshvir Rehill <dakshvirr@gmail.com>
2020-03-13 14:14:10 -0400

    Added NPCs and Prefabs and tree manager

--- Assets/Game/Scripts/Helpers/GenEvents.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
-
-public class SceneLoadedEvent : UnityEvent<List<string>> { }
--- Assets/Game/Scripts/SystemHelpers/MenuClassifier.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "MenuClassifier", menuName = "UI/Menu Classifier")]
-public class MenuClassifier : ScriptableObject
-{
-	public string mMenuName;
-}
